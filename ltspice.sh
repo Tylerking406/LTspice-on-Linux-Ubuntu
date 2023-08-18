787877de56a5295e6f6065996ca2aa3191caf772
Tylerking406 <82594687+Tylerking406@users.noreply.github.com>
2023-08-18 09:08:36 +0200

    Script for running LTSpice on the terminal

---
+++ ltspice.sh
@@ -0,0 +1,13 @@
+# install wine and ltspice
+sudo apt-get install wine
+# sudo pacman -S wine
+cd /tmp/
+wget http://ltspice.linear-tech.com/software/LTspiceIV.exe
+wine LTspiceIV.exe
+rm LTspiceIV.exe
+
+# start lstpice through wine
+wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceIV/scad3.exe
+
+# start ltspice through wine with /tmp/example.asc
+wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceIV/scad3.exe /tmp/example.asc
